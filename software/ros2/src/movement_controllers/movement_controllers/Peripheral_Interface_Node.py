432545de50e53e93f196adca543db226f91cbcd7
darudecookie <darudecookie@gmail.com>
2024-06-14 02:51:02 -0400

    properly configured ROS wkspc

---
+++ software/ros2/src/movement_controllers/movement_controllers/Peripheral_Interface_Node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from movement_controllers.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ software/ros2/src/ros_controller_interface/ros_controller_interface/ROS_Controller_Node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros_controller_interface.')
+
+
+if __name__ == '__main__':
+    main()
