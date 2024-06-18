827f057baa8504bf79c1dbf94a6c54b7248f539e
darudecookie <darudecookie@gmail.com>
2024-06-17 23:13:53 -0400

    added some stuff ifykyk

--- software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
+++ software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
@@ -5,5 +5,12 @@ from v2_robot_arm_interfaces.srv import TargetEEState
 
 class End_Effector_Controller(rclpy.Node):
     def __init__(self):
-        self.current_EE_Info_sub = self.create_subscription()
-        self.target_EE_info_pub = self.create_publisher(TargetEEInfo, )
+        self.current_EE_Info_sub = self.create_subscription(CurrentEEInfo, "current_end_effector_information", self.current_EE_callback)
+        self.target_EE_info_pub = self.create_publisher(TargetEEInfo,"target_end_effector_information", 10)
+
+        self.EE_State_server = self.create_subscription(TargetEEState, "target_end_effector_state", self.EE_state_callback)
+
+    def EE_state_callback(self, request, response):
+
+    def current_EE_callback(self, msg):
+        
