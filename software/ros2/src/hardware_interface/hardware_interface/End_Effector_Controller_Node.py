27cab3fe890a2dcddea4d434d00f17400e2ded52
darudecookie <darudecookie@gmail.com>
2024-06-18 19:05:10 -0400

    added geatures to ee and cartesian controller; modified messages to remove unuses sections

--- software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
+++ software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
@@ -1,24 +1,32 @@
 
 import rclpy
-from rclpy.node import Node
 from v2_robot_arm_interfaces.msg import CurrentEEInfo, TargetEEInfo 
 from v2_robot_arm_interfaces.srv import TargetEEState
 
-class End_Effector_Controller(Node):
+class End_Effector_Controller(rclpy.node.Node):
     def __init__(self):
         super().__init__("End_Effector_Controller_Node")
 
-        self.current_EE_Info_sub = self.create_subscription(CurrentEEInfo, "current_end_effector_information", self.current_EE_callback, 10)
+        self.internal_current_EE_state = 0
+        self.internal_target_EE_state = 0
+        
+        self.current_EE_Info_sub = self.create_subscription(CurrentEEInfo,"current_end_effector_information", self.current_EE_callback, 10)
         self.target_EE_info_pub = self.create_publisher(TargetEEInfo,"target_end_effector_information", 10)
 
         self.EE_State_server = self.create_service(TargetEEState, "target_end_effector_state", self.EE_state_callback)
 
     def EE_state_callback(self, request, response):
-        print(":3")
-        response.current_end_effector_value = 0.5
+        self.internal_target_EE_state=request.target_end_effector_value 
+        response.current_end_effector_value = self.internal_current_EE_state
         return response
+    
     def current_EE_callback(self, msg):
-        print("moew")
+        self.internal_current_EE_state = msg.current_end_effector_value
+    
+    def target_EE_timer_callback(self, msg):
+        msg = TargetEEInfo
+        msg.
+        self.target_EE_info_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
--- software/ros2/src/hardware_interface/hardware_interface/Microcontroller_Interface_Node.py
+++ software/ros2/src/hardware_interface/hardware_interface/Microcontroller_Interface_Node.py
@@ -3,7 +3,6 @@ import time
 from collections import deque
 
 import rclpy
-from rclpy.node import Node
 from v2_robot_arm_interfaces.msg import CurrentEEInfo, TargetEEInfo, CurrentJointInfo, TargetJointInfo, SystemDiagnosticInfo
 from v2_robot_arm_interfaces.srv import MicrocontrollerParameterDump
 
@@ -38,14 +37,14 @@ MCU_arguments = {
     41: "Set_Home_Joint_Positions",
 
         
-    50: "Get_EE_Boolean",
-    51: "Get_EE_Float",
-    52: "Set_EE_Boolean",
-    53: "Set_EE_Float",
+    50: "Get_EE_Float",
+    51: "Set_EE_Float",
+    
+    60: "Get_Temperatures",
 }
 
 
-class Serial_Interface(Node):
+class Serial_Interface(rclpy.node.Node):
     def __init__(
         self,
         port: str = "COM4",
@@ -84,7 +83,7 @@ class Serial_Interface(Node):
         self.System_Diagnostic_pub = self.create_publisher(SystemDiagnosticInfo, "system_diagnostic_information", 10)
 
         self.current_EE_Info_pub = self.create_publisher(CurrentEEInfo, "current_end_effector_information", 10)
-        self.current_EE_Info_components =[False, False]
+
         self.target_EE_Info_sub = self.create_subscription(TargetEEInfo, "target_end_effector_information", 10)
 
         # creating clients/servers for communication
@@ -94,18 +93,20 @@ class Serial_Interface(Node):
         # there are two callbacks, one for this node <-> MCU and one for this node <-> rest of ROS
         # value (hz) for program to read and then write an mcu message
         MCU_communication_frequency = 200
-        self.MCU_communication_timer = rclpy.Node.create_timer(1/MCU_communication_frequency, self.MCU_comminication_loop)
+        self.MCU_communication_timer = self.create_timer(1/MCU_communication_frequency, self.MCU_communication_loop)
 
         # value for messages to be pulled from queue and sent to ros
         ros_communication_frequency = 500
-        self.ros_communication_timer = rclpy.Node.create_timer(1/ros_communication_frequency, self.ros_communication_loop)
+        self.ros_communication_timer = self.create_timer(1/ros_communication_frequency, self.ros_reporting_loop)
+
+        self.get_logger().info("MCU_Interface_Node setup complete")
 
     def __del__(self):
         self.Serial_port.close()
 
+
     def key_from_val(self, value: str) -> int:
         return self.MCU_key_list[self.MCU_val_list.index(value)]
-
 
     def decode_from_serial(self, serial_input: str) -> tuple:
         should_parse = False
@@ -155,10 +156,8 @@ class Serial_Interface(Node):
                     float_sections[i] = abs(float_sections[i]) + 101
             return (
                 float_sections[0].to_bytes(1, byteorder="little", signed=False)
-                + float_sections[1].to_bytes(1,
-                                             byteorder="little", signed=False)
-                + float_sections[2].to_bytes(1,
-                                             byteorder="little", signed=False)
+                + float_sections[1].to_bytes(1, byteorder="little", signed=False)
+                + float_sections[2].to_bytes(1, byteorder="little", signed=False)
             )
         else:
             self.get_logger().debug("float value out of range; val not converted")
@@ -197,9 +196,9 @@ class Serial_Interface(Node):
             float_array.append(self.decode_1_float(input_bytes[i:i+2]))
 
         return float_array
-    
 
-    def MCU_comminication_loop(self):
+
+    def MCU_communication_loop(self):
         read_line = self.Serial_port.readline()
         if read_line:
             self.MCU_report_queue.append(self.decode_from_serial(read_line))
@@ -210,16 +209,9 @@ class Serial_Interface(Node):
 
         self.get_logger().info(f"report queue: {len(self.MCU_report_queue)}, send queue: {len(self.MCU_send_queue)}")
 
-    def ros_communication_loop(self):
-        if len(self.MCU_report_queue > 0):
-            information_to_report = self.MCU_report_queue.popleft()
-            if information_to_report[0] == -1:
-                return
-            match information_to_report[0]
-    
     def send_system_status(self, request, response):
         if request.estop != 0:
-            estop_byte_msg = self.command_to_bytes(MCU_arguments["Estop"])
+            estop_byte_msg = self.command_to_bytes(self.key_from_val("Estop"))
             if request.estop == 1:
                 estop_byte_msg += self.command_to_bytes(1)
             elif request.estop == -1:
@@ -227,7 +219,7 @@ class Serial_Interface(Node):
             self.MCU_send_queue.append(estop_byte_msg)
 
         if request.jointhold != 0:
-            estop_byte_msg = self.command_to_bytes(MCU_arguments["JointHold"])
+            estop_byte_msg = self.command_to_bytes(self.key_from_val("JointHold"))
             if request.jointhold == 1:
                 estop_byte_msg += self.command_to_bytes(1)
             elif request.jointhold == -1:
@@ -236,7 +228,7 @@ class Serial_Interface(Node):
 
         if request.move_home == 1:
             self.MCU_send_queue.append(
-                self.command_to_bytes(MCU_arguments["Move_Home"]))
+                self.command_to_bytes(self.key_from_val("Move_Home")))
 
         response.returnsuccess = 1
 
@@ -247,16 +239,13 @@ class Serial_Interface(Node):
         float_vals_to_write = []
 
         if msg.param_to_control == 0:
-            joint_info_msg += self.command_to_bytes(
-                MCU_arguments["Set_Joint_Positions"])
+            joint_info_msg += self.command_to_bytes(self.key_from_val("Set_Joint_Positions"))
             float_vals_to_write = msg.target_joint_velocities
         elif msg.param_to_control == 1:
-            joint_info_msg += self.command_to_bytes(
-                MCU_arguments["Set_Joint_Velocities"])
+            joint_info_msg += self.command_to_bytes(self.key_from_val("Set_Joint_Velocities"))
             float_vals_to_write = msg.Target_Joint_Velocities
         elif msg.param_to_control == 2:
-            joint_info_msg += self.command_to_bytes(
-                MCU_arguments["Set_Joint_Torques"])
+            joint_info_msg += self.command_to_bytes(self.key_from_val("Set_Joint_Torques"))
             float_vals_to_write = msg.target_joint_torques
 
         joint_info_msg += self.encode_n_floats(float_vals_to_write, 7)
@@ -264,60 +253,40 @@ class Serial_Interface(Node):
         self.MCU_send_queue.append(joint_info_msg)
 
     def send_end_effector_information(self, msg):
-        if msg.target_end_effector_bool != 0:
-            if msg.target_end_effector_bool == 1:
-                end_effector_val = 1
-            elif msg.target_end_effector_bool == -1:
-                end_effector_val = 0
-            self.MCU_send_queue.append(self.command_to_bytes(
-                MCU_arguments["Set_EE_Boolean"]) + self.command_to_bytes(end_effector_val))
-
-        else:
-            end_effector_val = msg.target_end_effector_value
-            self.MCU_send_queue.append(self.command_to_bytes(
-                MCU_arguments["Set_EE_Float"]) + self.encode_1_float(end_effector_val))
+        end_effector_val = msg.target_end_effector_value
+        self.MCU_send_queue.append(self.command_to_bytes(self.key_from_val("Set_EE_Float")) + self.encode_1_float(end_effector_val))
 
     def send_MCU_parameter_dump(self, request, response) -> int:
         def queue_param_if_needed(target_joint: int, param_values: float, param_code_name: str, ) -> None:
             if target_joint != - 10:
                 encoded_msg = rb''
                 encoded_msg = self.command_to_bytes(
-                    MCU_arguments[param_code_name])
+                    self.key_from_val(param_code_name))
                 encoded_msg += self.command_to_bytes(target_joint)
                 encoded_msg += self.encode_n_floats(
                     param_values, len(param_values))
             self.MCU_send_queue.append(encoded_msg)
 
-        queue_param_if_needed(request.joint_position_limit_update_target,
-                              request.joint_position_limit_update_params, "Set_Joint_Position_Limits")
-        queue_param_if_needed(request.joint_velocity_limit_update_target,
-                              request.joint_velocity_limit_update_params, "Set_Joint_Velocity_Limits")
-        queue_param_if_needed(request.joint_torque_limit_update_target,
-                              request.joint_torque_limit_update_params, "Set_Joint_Torque_Limits")
-        queue_param_if_needed(request.joint_acceleration_limit_update_target,
-                              request.joint_acceleration_limit_update_params, "Set_Joint_Acceleration_Limits")
-        queue_param_if_needed(request.joint_jerk_limit_update_target,
-                              request.joint_jerk_limit_update_params, "Set_Joint_Jerk_Limits")
+        queue_param_if_needed(request.joint_position_limit_update_target, request.joint_position_limit_update_params, "Set_Joint_Position_Limits")
+        queue_param_if_needed(request.joint_velocity_limit_update_target, request.joint_velocity_limit_update_params, "Set_Joint_Velocity_Limits")
+        queue_param_if_needed(request.joint_torque_limit_update_target, request.joint_torque_limit_update_params, "Set_Joint_Torque_Limits")
+        queue_param_if_needed(request.joint_acceleration_limit_update_target, request.joint_acceleration_limit_update_params, "Set_Joint_Acceleration_Limits")
+        queue_param_if_needed(request.joint_jerk_limit_update_target, request.joint_jerk_limit_update_params, "Set_Joint_Jerk_Limits")
 
-        queue_param_if_needed(request.PID_update_target,
-                              request.PID_update_params, "Set_Target_Joint_PID_Params")
+        queue_param_if_needed(request.PID_update_target, request.PID_update_params, "Set_Target_Joint_PID_Params")
 
         def queue_workspace_bound_if_needed(workspace_bound: float[2], param_code_name: str) -> None:
             if any(workspace_bound) != -1:
-                encoded_msg = self.command_to_bytes(
-                    MCU_arguments[param_code_name])
+                encoded_msg = self.command_to_bytes(self.key_from_val(param_code_name))
                 encoded_msg += self.encode_n_floats(workspace_bound)
                 self.MCU_send_queue.append(encoded_msg)
 
-        queue_workspace_bound_if_needed(
-            request.x_workspace_bounds, "Set_X_Workspace_Limit")
-        queue_workspace_bound_if_needed(
-            request.y_workspace_bounds, "Set_Y_Workspace_Limit")
-        queue_workspace_bound_if_needed(
-            request.z_workspace_bounds, "Set_Z_Workspace_Limit")
+        queue_workspace_bound_if_needed(request.x_workspace_bounds, "Set_X_Workspace_Limit")
+        queue_workspace_bound_if_needed(request.y_workspace_bounds, "Set_Y_Workspace_Limit")
+        queue_workspace_bound_if_needed(request.z_workspace_bounds, "Set_Z_Workspace_Limit")
 
         if any(request.joint_home_positions) != -1:
-            encoded_msg = self.command_to_bytes(MCU_arguments["Set_Home_Joint_Positions"])
+            encoded_msg = self.command_to_bytes(self.key_from_val("Set_Home_Joint_Positions"))
             encoded_msg += self.encode_n_floats(request.joint_home_positions, 7)
             self.MCU_send_queue.append(encoded_msg)
 
@@ -325,23 +294,60 @@ class Serial_Interface(Node):
         return response
 
 
-    def report_system_diagnostic_information(self, passed_arg: str) -> None:
-        print("mwo")
+    def ros_reporting_loop(self):
+        if len(self.MCU_report_queue > 0):
+            information_to_report = self.MCU_report_queue.popleft()
+            if information_to_report[0] != -1:   
+                match MCU_arguments[information_to_report[0]]:
+                    case "Estop":
+                        self.report_system_diagnostic_information(information_to_report[0], information_to_report[1])
+                    case "JointHold":
+                        self.report_system_diagnostic_information(information_to_report[0], information_to_report[1])
+                        
+                    case  "Get_Joint_Positions":
+                        self.report_joint_information(information_to_report[0], information_to_report[1])
+                    case  "Get_Joint_Velocities":
+                        self.report_joint_information(information_to_report[0], information_to_report[1])
+                    case  "Get_Joint_Torques":
+                        self.report_joint_information(information_to_report[0], information_to_report[1])
+                    case  "Get_Joint_Accelerations":
+                        self.report_joint_information(information_to_report[0], information_to_report[1])
+                    case  "Get_Joint_Jerks":
+                        self.report_joint_information(information_to_report[0], information_to_report[1])
+
+                    case  "Get_EE_Float":
+                        self.report_end_effector_information(information_to_report[1])
+
+    def report_system_diagnostic_information(self, passed_cmd: int, passed_arg: str) -> None:
+        argument = int.from_bytes(passed_arg, "little")
+
+        if passed_cmd == self.key_from_val("Estop"):
+            msg = SystemDiagnosticInfo
+            if argument == 1:
+                msg.estop = 1
+            elif argument == 0:
+                msg.estop = -1
+                
+        elif passed_cmd == self.key_from_val("Get_Temperatures"):
+            msg = SystemDiagnosticInfo
+            msg.temperatures = self.decode_n_floats(passed_arg, 7)
+            
+        self.System_Diagnostic_pub.publish(msg)
 
     def report_joint_information(self, passed_cmd: int, passed_arg: str) -> None:
-        if passed_cmd == MCU_arguments["Get_Joint_Positions"]:
+        if passed_cmd == self.key_from_val("Get_Joint_Positions"):
             self.current_Joint_Info_components[0] = self.decode_n_floats(input_bytes = passed_arg, n_floats=7)
             
-        elif passed_cmd == MCU_arguments["Get_Joint_Velocities"]:
+        elif passed_cmd == self.key_from_val("Get_Joint_Velocities"):
             self.current_Joint_Info_components[1] = self.decode_n_floats(input_bytes = passed_arg, n_floats=7)
 
-        elif passed_cmd == MCU_arguments["Get_Joint_Torques"]:
+        elif passed_cmd == self.key_from_val("Get_Joint_Torques"):
             self.current_Joint_Info_components[2] = self.decode_n_floats(input_bytes = passed_arg, n_floats=7)
 
-        elif passed_cmd == MCU_arguments["Get_Joint_Accelerations"]:
+        elif passed_cmd == self.key_from_val("Get_Joint_Accelerations"):
             self.current_Joint_Info_components[3] = self.decode_n_floats(input_bytes = passed_arg, n_floats=7)
 
-        elif passed_cmd == MCU_arguments["Get_Joint_Jerks"]:
+        elif passed_cmd == self.key_from_val("Get_Joint_Jerks"):
             self.current_Joint_Info_components[4] = self.decode_n_floats(input_bytes = passed_arg, n_floats=7)
 
         if len(all(self.current_Joint_Info_components)) != 0:
@@ -356,23 +362,13 @@ class Serial_Interface(Node):
             self.current_Joint_Info_pub.publish(msg)
             
             self.current_Joint_Info_components =  [[], [], [], [], []]
-            
-    def report_end_effector_information(self, passed_cmd: int, passed_arg: str) -> None:
-        if passed_cmd == MCU_arguments["Get_EE_Boolean"]:
-            self.current_EE_Info_components[0]  = self.decode_from_serial(int.from_bytes(passed_arg, "little"))
-            
-        elif passed_cmd == MCU_arguments["Get_EE_Float"]:
-            self.current_EE_Info_components[1] = self.decode_1_float(passed_arg)
 
-        if type(all(self.current_EE_Info_components))!=bool and all(self.current_EE_Info_components)!=False:
-            msg = CurrentEEInfo
-            
-            msg.current_end_effector_bool = self.current_EE_Info_components[0]
-            msg.current_end_effector_value = self.current_EE_Info_components[1]
-            
-            self.current_EE_Info_pub.publish(msg)
-            
-            self.current_EE_Info_components =[False, False]
+    def report_end_effector_information(self,  passed_arg: str) -> None:
+        msg = CurrentEEInfo
+        
+        msg.current_end_effector_value =  self.decode_1_float(passed_arg)
+        
+        self.current_EE_Info_pub.publish(msg)
 
 
 def main(args=None):
