0a98e140456f7b3f17e46f397507ce7b75365527
darudecookie <darudecookie@gmail.com>
2024-06-17 21:14:50 -0400

    updated ee controller node

--- software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
+++ software/ros2/src/hardware_interface/hardware_interface/End_Effector_Controller_Node.py
@@ -1,5 +1,9 @@
+
 import rclpy
 from v2_robot_arm_interfaces.msg import CurrentEEInfo, TargetEEInfo 
 from v2_robot_arm_interfaces.srv import TargetEEState
 
-
+class End_Effector_Controller(rclpy.Node):
+    def __init__(self):
+        self.current_EE_Info_sub = self.create_subscription()
+        self.target_EE_info_pub = self.create_publisher(TargetEEInfo, )
