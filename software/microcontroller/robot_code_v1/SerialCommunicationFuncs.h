a1fb4fffd2b7fd45d4ba7e6b6798e9e1901a966b
darudecookie <darudecookie@gmail.com>
2024-06-29 01:20:36 -0400

    updated serial comm funcs

--- software/microcontroller/robot_code_v1/SerialCommunicationFuncs.h
+++ software/microcontroller/robot_code_v1/SerialCommunicationFuncs.h
@@ -1,12 +1,17 @@
 #ifndef Serial_Communications_Funcs_h
 #define Serial_Communications_Funcs_h
 
+
 extern float decode_1_float(unsigned char byte_array[3]);
 
 extern void decode_7_floats(float target_array[7], unsigned char input_bytes[21]);
 
+extern void get_string_from_serial();
+
+
 extern void encode_1_float(float input_float, unsigned char modified_bytes[3]);
 
 extern void encode_7_floats(float input_float_aray[7], unsigned char modifid_bytes[21]);
+
 
 #endif
