8bc991ab2696042554f3f6c865884a4375c0288f
darudecookie <114975020+darudecookie@users.noreply.github.com>
2024-07-03 01:44:52 -0400

    Delete software/ros2/src/build directory

--- software/ros2/src/build/hardware_interface/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/darudecookie/Documents/V2-Robot-Arm/software/ros2/src/install/v2_robot_arm_interfaces/share/v2_robot_arm_interfaces/package.sh"
