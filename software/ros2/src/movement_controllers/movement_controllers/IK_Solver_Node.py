76b5d88ae51d333d748fc4ab16311a8ec6ef7af9
darudecookie <darudecookie@gmail.com>
2024-07-18 14:47:40 -0400

    fixed spelling errors

--- software/ros2/src/movement_controllers/movement_controllers/IK_Solver_Node.py
+++ software/ros2/src/movement_controllers/movement_controllers/IK_Solver_Node.py
@@ -4,64 +4,59 @@ import rclpy
 from rclpy.node import Node
 from v2_robot_arm_interfaces.msg import CurrentJointInfo, TargetJointInfo, CurrentCartesian, TargetCartesian, ControlStatus
 
+
 class IK_Controller(Node):
+
     def __init__(self):
         super().__init__("IK_Solver_Node") 
         self.get_logger().info("initiating node")
 
         self.should_output = False
 
-
-        self.target_joint_angles = [0,0,0,0,0,0,0]
-        self.current_joint_angles = [0,0,0,0,0,0,0]
+        self.target_joint_angles = [0, 0, 0, 0, 0, 0, 0]
+        self.current_joint_angles = [0, 0, 0, 0, 0, 0, 0]
 
         self.target_cartesian_matrix = PyKDL.Frame()
         self.translation_speed = 0
         self.rotation_speed = 0
 
         self.current_cartesian_matrix = PyKDL.Frame()
-        
 
-        self.Current_Status_sub = self.create_subscription(ControlStatus, "control_status", self.recieve_output_status, 10)
+        self.Current_Status_sub = self.create_subscription(ControlStatus, "control_status", self.receive_output_status, 10)
 
-        self.Current_Joint_sub = self.create_subscription(CurrentJointInfo, "current_joint_information", self.recieve_current_joint_angles, 10)
+        self.Current_Joint_sub = self.create_subscription(CurrentJointInfo, "current_joint_information", self.receive_current_joint_angles, 10)
         self.Target_Joint_pub = self.create_publisher(TargetJointInfo, "target_joint_information", 10)
 
         self.Current_Cartesian_pub = self.create_publisher(CurrentCartesian, "current_cartesian", 10)
-        self.Target_Cartesian_sub = self.create_subscription(TargetCartesian, "target_cartesian", self.recieve_target_cartesian, 10)
-
+        self.Target_Cartesian_sub = self.create_subscription(TargetCartesian, "target_cartesian", self.receive_target_cartesian, 10)
 
         self.IK_Calc_frequency = 200
-        self.IK_timer = self.create_timer(1/self.IK_Calc_frequency, self.calulate_IK_publish_JointAngles)
+        self.IK_timer = self.create_timer(1 / self.IK_Calc_frequency, self.calculate_IK_publish_JointAngles)
 
-
-    def recieve_output_status(self, msg):
+    def receive_output_status(self, msg):
         if msg.ik_solver == 1:
             self.should_output = True
         elif msg.ik_solver == 0:
             self.should_output = False
 
-
-    def recieve_current_joint_angles(self, msg):
+    def receive_current_joint_angles(self, msg):
         self.current_joint_angles = msg.positions
         self.calculate_FK_publish_CurrentCartesian()
 
     def calculate_FK_publish_CurrentCartesian(self):
        
-       
         msg = CurrentCartesian()
 
         self.Current_Cartesian_pub.publish(msg)
 
-
-    def recieve_target_cartesian(self, msg):
+    def receive_target_cartesian(self, msg):
         self.target_cartesian_matrix.p = msg.position 
         self.target_cartesian_matrix.M = msg.rotation
 
         self.translation_speed = msg.translation_speed
         self.rotation_speed = msg.rotation_speed
 
-    def calulate_IK_publish_JointAngles(self):
+    def calculate_IK_publish_JointAngles(self):
         if self.should_output:
             msg = TargetJointInfo()
             msg.param_to_control = 0
